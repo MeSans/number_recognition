3b1c2feaeddee2970ee2fcf207a66eb8b0cb32d3
MeSans <patriks.misans@gmail.com>
2017-09-05 14:03:58 +0300

    added not working code array visualization

--- pixel_array_bullshittery.py
+++ pixel_array_bullshittery.py
@@ -2,7 +2,7 @@
 from PIL import Image
 import numpy as np
 # open up the image
-i = Image.open('images/dotnot.png')
+i = Image.open('images/floy.png')
 # convert our image to a pixel array
 iar = np.asarray(i)
 # print that stuff out
