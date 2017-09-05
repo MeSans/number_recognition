1faec7441c976df81db0dc0b842776f4d21b0841
MeSans <patriks.misans@gmail.com>
2017-09-05 13:49:55 +0300

    cloned the images and printed conversion to pixelarray

---
+++ pixel_array_bullshittery.py
@@ -0,0 +1,9 @@
+# 64 bit with pillow:
+from PIL import Image
+import numpy as np
+# open up the image
+i = Image.open('images/dotnot.png')
+# convert our image to a pixel array
+iar = np.asarray(i)
+# print that stuff out
+print(iar)
